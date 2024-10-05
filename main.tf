5653c06f21a96cd95050d7a06a7d815f45c7ad06
hexoren21 <hexoren@gmail.com>
2024-10-05 11:36:27 +0200

    added sg

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ module "networking" {
     cidr_private_subnet = var.cidr_private_subnet
     eu_availability_zone = var.eu_availability_zone
 }
+
+module "security_group" {
+    source              = "./Security-groups"
+    ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+    vpc_id              = module.networking.dev_proj_vpc_id
+    ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+}
