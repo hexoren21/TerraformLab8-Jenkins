a80e8faf7731bafa8a965fad1da8cc4c92130de5
hexoren21 <hexoren@gmail.com>
2024-10-17 16:59:57 +0200

    added lb and https

--- Security-groups/main.tf
+++ Security-groups/main.tf
@@ -2,6 +2,7 @@ variable "ec2_sg_name" {}
 variable "vpc_id" {}
 variable "ec2_jenkins_sg_name" {}
 variable "ip_address" {}
+variable "lb_sg_name" {}
 
 output "sg_ec2_sg_ssh_http_id" {
     value = aws_security_group.ec2_sg_ssh_http.id
@@ -9,6 +10,10 @@ output "sg_ec2_sg_ssh_http_id" {
 
 output "sg_ec2_jenkins_port_8080" {
     value = aws_security_group.ec2_jenkins_port_8080.id
+}
+
+output "lb_sg_ssh_http_id" {
+    value = aws_security_group.lb_sg_ssh_http.id
 }
 
 resource "aws_security_group" "ec2_sg_ssh_http" {
@@ -33,6 +38,15 @@ resource "aws_security_group" "ec2_sg_ssh_http" {
     to_port     = 80
     protocol    = "tcp"
   }
+
+  ingress {
+    description = "Allow HTTP request from lb"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg_ssh_http.id]
+  }
+
 
   # enable http
   ingress {
@@ -73,5 +87,42 @@ resource "aws_security_group" "ec2_jenkins_port_8080" {
 
   tags = {
        Name = "Security Groups to allow SSH(22) and HTTP(80)"
+  }
+}
+
+resource "aws_security_group" "lb_sg_ssh_http" {
+  name        = var.lb_sg_name
+  description = "Enable the Port 80 and 443 for lb"
+  vpc_id      = var.vpc_id
+
+  # enable http
+  ingress {
+    description = "Allow HTTP request from my ip"
+    cidr_blocks = [var.ip_address]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  # enable http
+  ingress {
+    description = "Allow HTTP request from ip"
+    cidr_blocks = [var.ip_address]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+
+  #Outgoing request
+  egress {
+    description = "Allow outgoing request"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security Groups lb to allow HTTP(80) and HTTPS(443)"
   }
 }
--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "security_group" {
   vpc_id              = module.networking.dev_proj_vpc_id
   ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
   ip_address          = var.my_ip
+  lb_sg_name = "SG for lb to enable HTTPS(443) and HTTP(80)"
 }
 
 module "jenkins" {
@@ -25,4 +26,45 @@ module "jenkins" {
   sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
   enable_public_ip_address  = true
   user_data_install_jenkins = templatefile("./Jenkins-runner-script/jenkins-installer.sh", {})
+}
+
+module "lb_target_group" {
+  source                   = "./load-balancer-target-group"
+  lb_target_group_name     = "jenkins-lb-target-group"
+  lb_target_group_port     = 8080
+  lb_target_group_protocol = "HTTP"
+  vpc_id                   = module.networking.dev_proj_vpc_id
+  ec2_instance_id          = module.jenkins.jenkins_ec2_instance_ip
+}
+
+module "alb" {
+  source                    = "./load-balancer"
+  lb_name                   = "dev-proj-alb"
+  is_external               = false
+  lb_type                   = "application"
+  sg_enable_ssh_https       = module.security_group.lb_sg_ssh_http_id
+  subnet_ids                = tolist(module.networking.dev_proj_public_subnets)
+  tag_name                  = "dev-proj-alb"
+  lb_target_group_arn       = module.lb_target_group.dev_proj_lb_target_group_arn
+  ec2_instance_id           = module.jenkins.jenkins_ec2_instance_ip
+  lb_listner_port           = 80
+  lb_listner_protocol       = "HTTP"
+  lb_listner_default_action = "forward"
+  lb_https_listner_port     = 443
+  lb_https_listner_protocol = "HTTPS"
+  dev_proj_acm_arn        = module.aws_ceritification_manager.dev_proj_acm_arn
+  lb_target_group_attachment_port = 8080
+}
+
+module "hosted_zone" {
+  source          = "./hosted-zone"
+  domain_name     = "jenkins.kube.misjadevops.com"
+  aws_lb_dns_name = module.alb.aws_lb_dns_name
+  aws_lb_zone_id  = module.alb.aws_lb_zone_id
+}
+
+module "aws_ceritification_manager" {
+  source         = "./certificate-manager"
+  domain_name    = "jenkins.kube.misjadevops.com"
+  hosted_zone_id = module.hosted_zone.hosted_zone_id
 }
